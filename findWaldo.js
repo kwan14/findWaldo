1a736b3f2831614fa407658d83e1b32a84dd664a
kwan14 <mendel.kwan@telus.net>
2017-09-07 18:51:34 +0000

    Found Waldo using forEach

--- findWaldo.js
+++ findWaldo.js
@@ -2,11 +2,12 @@
 
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
+  /*for (var i = 0; i < arr.length; i++) {
     if (arr[i] === "Waldo") {
       found(i);   // execute callback
     }
-  }
+  */
+  arr.forEach(function(element, i) { if (element === "Waldo") { found(i); } } );
 }
 
 function actionWhenFound(index) {
